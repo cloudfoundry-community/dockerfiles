104919a4dbdf1d26f43284e905cc2b05c09d8ce8
James Hunt <james@niftylogic.com>
2016-02-07 20:44:33 +0000

    Cleanup after aws-cli install

--- concourse/latest/scripts/install_aws_cli.sh
+++ concourse/latest/scripts/install_aws_cli.sh
@@ -19,3 +19,6 @@ source ~/.profile
 curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
 unzip awscli-bundle.zip
 ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+
+# cleanup
+rm -rf awscli-* /tmp/*
