3455171925e3d5d476fb925823dae4f286b8f4ee
Dr Nic Williams <drnicwilliams@gmail.com>
2019-08-16 10:57:21 +1000

    test wildcard into script

--- ci/scripts/latest-from-buildpack.sh
+++ ci/scripts/latest-from-buildpack.sh
@@ -2,12 +2,13 @@
 
 set -eu
 
-manifest=$1   # path/to/buildpack/manifest.yml
+buildpack=$1   # path/to/buildpack.zip
 dependency=$2 # e.g. go, ruby, java
 version_prefix=$3 # e.g. 1.12.
-[[ -f $manifest ]] || { echo "pass path to manifest.yml"; exit 1; }
+[[ -f $buildpack ]] || { echo "pass path to buildpack.zip"; exit 1; }
 
-dependencies=$(spruce json $manifest | jq -r --arg dep $dependency '.dependencies | map(select(.name == $dep))')
+manifest=$(unzip -p $buildpack manifest.yml)
+dependencies=$(spruce json <(echo "$manifest") | jq -r --arg dep $dependency '.dependencies | map(select(.name == $dep))')
 latest_version=$version_prefix$(echo "$dependencies" | jq -r ".[].version | scan(\"^$version_prefix(.*)\")[0]" | sort -r | head -n 1)
 latest_dep=$(echo "$dependencies" | jq -r --arg dep $dependency --arg version $latest_version '.[] | select(.name == $dep and .version == $version)')
 echo "ENV GOLANG_VERSION $latest_version"
