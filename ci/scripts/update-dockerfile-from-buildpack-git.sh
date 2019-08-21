a1e4ab41479d7b9371148c92fc219942891c723b
Dr Nic Williams <drnicwilliams@gmail.com>
2019-08-22 09:18:44 +1000

    only update Dockerfile if manifest.yml contains the major.minor version

--- ci/scripts/update-dockerfile-from-buildpack-git.sh
+++ ci/scripts/update-dockerfile-from-buildpack-git.sh
@@ -11,7 +11,7 @@ dockerfile_name=${dockerfile_name:-$dependency}
 
 manifest=${buildpack}/manifest.yml
 [[ -f $manifest ]] || { echo "No $manifest found"; exit 1; }
-dependencies=$(spruce json <(echo "$manifest") | jq -r --arg dep $dependency '.dependencies | map(select(.name == $dep))')
+dependencies=$(spruce json $manifest | jq -r --arg dep $dependency '.dependencies | map(select(.name == $dep))')
 
 git clone $root pushme
 cd pushme
@@ -23,25 +23,28 @@ if [[ -z $(git config --global user.name) ]]; then
   git config --global user.name "${git_name}"
 fi
 
-exit 1
 for dockerfile in $(ls -d concourse-$dockerfile_name/*/Dockerfile); do
   for version_prefix in $(basename $(dirname $dockerfile)); do
     echo "Checking $version_prefix of $dockerfile"
-    latest_version=$version_prefix$(echo "$dependencies" | jq -r ".[].version | scan(\"^$version_prefix(.*)\")[0]" | sort -r | head -n 1)
-    echo "Latest version $latest_version"
-    latest_dep=$(echo "$dependencies" | jq -r --arg dep $dependency --arg version $latest_version '.[] | select(.name == $dep and .version == $version)')
+    patch_release=$(echo "$dependencies" | jq -r ".[].version | scan(\"^$version_prefix(.*)\")[0]" | sort -r | head -n 1)
+    # do not touch Dockerfile if manifest.yml does not yet/no longer includes versions
+    if [[ "${patch_release:-X}" != "X" ]]; then
+      latest_version=$version_prefix$patch_release
+      echo "Latest version $latest_version"
+      latest_dep=$(echo "$dependencies" | jq -r --arg dep $dependency --arg version $latest_version '.[] | select(.name == $dep and .version == $version)')
 
-    sed -i "s%^ENV ${dockerfile_dep_prefix}_VERSION.*$%ENV ${dockerfile_dep_prefix}_VERSION $latest_version%" $dockerfile
-    sed -i "s%^ENV ${dockerfile_dep_prefix}_URL.*$%ENV ${dockerfile_dep_prefix}_URL $(echo "$latest_dep" | jq -r ".uri")%" $dockerfile
-    sed -i "s%^ENV ${dockerfile_dep_prefix}_SHA256.*$%ENV ${dockerfile_dep_prefix}_SHA256 $(echo "$latest_dep" | jq -r ".sha256")%" $dockerfile
+      sed -i "s%^ENV ${dockerfile_dep_prefix}_VERSION.*$%ENV ${dockerfile_dep_prefix}_VERSION $latest_version%" $dockerfile
+      sed -i "s%^ENV ${dockerfile_dep_prefix}_URL.*$%ENV ${dockerfile_dep_prefix}_URL $(echo "$latest_dep" | jq -r ".uri")%" $dockerfile
+      sed -i "s%^ENV ${dockerfile_dep_prefix}_SHA256.*$%ENV ${dockerfile_dep_prefix}_SHA256 $(echo "$latest_dep" | jq -r ".sha256")%" $dockerfile
 
-    if [[ "$(git status -s)X" != "X" ]]; then
-      echo "Detected new $dependency $latest_version"
-      git merge --no-edit master
-      git status
-      git --no-pager diff
-      git add $dockerfile
-      git commit -m "Updated $dependency $latest_version"
+      if [[ "$(git status -s)X" != "X" ]]; then
+        echo "Detected new $dependency $latest_version"
+        git merge --no-edit master
+        git status
+        git --no-pager diff
+        git add $dockerfile
+        git commit -m "Updated $dependency $latest_version"
+      fi
     fi
   done
 done
